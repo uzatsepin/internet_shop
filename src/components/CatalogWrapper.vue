fd6205eea1c4d8ee2253b9f22134e1aed5658e15
Andrii Zatsepin <uzatsepin@gmail.com>
2023-01-26 21:10:59 +0200

    initial: first commit, creating component Catalog, all properties comp

---
+++ src/components/CatalogWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="catalog-wrapper">
+    <p>{{title}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CatalogWrapper",
+    components: {},
+    props: {},
+    data() {
+      return {
+        title: 'Main Wrapper'
+      }
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    mounted() {
+      console.log("Тест")
+    }
+  }
+</script>
+
+<style>
+  .catalog-wrapper {
+    width: 900px;
+    margin: 0 auto;
+  }
+</style>
